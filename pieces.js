bef9dd38c1bbf49361f1ebc38d04bfd0752a17e4
Chloe Alexandre <chloe.alexandre@ynov.com>
2025-03-21 10:29:45 +0100

    refactor pieces class to include properties and methods for piece management

--- pieces.js
+++ pieces.js
@@ -1,3 +1,23 @@
-class pieces {
-    
+class Pieces {
+    static name = "";
+    static color = "";
+    static position = [];
+    static move = [];
+    static isCheck = false;
+    static isCheckmate = false;
+    static isStalemate = false;
+
+    constructor(name, color, position, move, isCheck, isCheckmate, isStalemate) {
+        this.name = name;
+        this.color = color;
+        this.position = position;
+        this.move = move;
+        this.isCheck = isCheck;
+        this.isCheckmate = isCheckmate;
+        this.isStalemate = isStalemate;
+    }
+
+    static getPiece() {
+        return this;
+    }
 }

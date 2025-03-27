b6685ee34be10e85a7b27b00d7748cd7312409b7
adrian.jaworski@ynov.com <adrian.jaworski@ynov.com>
2025-03-27 11:55:31 +0100

    chargemtn page

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+//node serveur.js
+//http://localhost:8080/
+
 const http = require('http')
 const port = 8080
 
 const requestHandler = (request, response) => {
     console.log(request.url)
-    response.end("Running")
+    const filePath = path.join(__dirname, 'index.html')
 }
 
 const server = http.createServer(requestHandler)

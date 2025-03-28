888aa83bec018ff4d2a27fb4359eeb5bf307163e
Chloe Alexandre <chloe.alexandre@ynov.com>
2025-03-28 11:15:22 +0100

    refactor Pawn class to improve move logic and implement getPossibleMoves method for enhanced move validation

--- pawn.js
+++ pawn.js
@@ -1,52 +1,43 @@
-class pawn extends piece {
-    constructor(x, y, color) {
-        super(x, y, color);
-        this.type = "pawn";
-        this.firstMove = true;
-        this.symbol = (color === "white") ? "♙" : "♟";
+class Pawn extends Piece {
+    constructor(color, position, board) {
+        super(color, position, board, 'pawn');
     }
 
-    move(x, y) {
-        if (this.firstMove) {
-            if (this.color === "white") {
-                if (y === this.y + 2 && x === this.x) {
-                    this.firstMove = false;
-                    return true;
-                }
-            } else {
-                if (y === this.y - 2 && x === this.x) {
-                    this.firstMove = false;
-                    return true;
-                }
-            }
-        }
-        if (this.color === "white") {
-            if (y === this.y + 1 && x === this.x) {
-                this.firstMove = false;
-                return true;
-            }
-        } else {
-            if (y === this.y - 1 && x === this.x) {
-                this.firstMove = false;
-                return true;
-            }
-        }
-        return false;
-    }
+    getPossibleMoves() {
+        const moves = [];
+        const direction = this.color === 'white' ? -1 : 1;
 
-    attack(x, y) {
-        if (this.color === "white") {
-            if (y === this.y + 1 && (x === this.x + 1 || x === this.x - 1)) {
-                this.firstMove = false;
-                return true;
-            }
-        } else {
-            if (y === this.y - 1 && (x === this.x + 1 || x === this.x - 1)) {
-                this.firstMove = false;
-                return true;
+        // Move forward
+        const oneStep = { x: this.position.x + direction, y: this.position.y };
+        if (this.board.isSquareEmpty(oneStep)) {
+            moves.push(oneStep);
+            // Move two steps forward from starting position
+            if(!this.hasMoved) {
+                const twoSteps = { x: this.position.x + direction * 2, y: this.position.y };
+                if (this.board.isSquareEmpty(twoSteps)) {
+                    moves.push(twoSteps);
+                }
             }
         }
-        return false;
+
+        // Capture diagonally
+        const takeDirections = [
+            {x: direction, y: 1},
+            {x: direction, y: -1}
+        ]
+
+        for (const dir of takeDirections) {
+            const takePos  = {
+                x: this.position.x + dir.x,
+                y: this.position.y + dir.y
+            };
+
+            if (this.board.isPositionValid(takePos) && 
+                !this.board.isSquareEmpty(takePos) && 
+                this.board.squares[takePos.x][takePos.y].color !== this.color) {
+                moves.push(takePos);
+            }
+        }
+        return moves;
     }
 }
-module.exports = pawn;

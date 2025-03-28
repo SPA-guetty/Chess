26d8e6a2296d3e4be94d603fa9d95ba592e2ea9e
Chloe Alexandre <chloe.alexandre@ynov.com>
2025-03-28 11:55:39 +0100

    refactor King class to streamline constructor and implement getPossibleMoves method for enhanced move validation and castling logic

--- king.js
+++ king.js
@@ -1,19 +1,64 @@
 class king extends piece {
-    constructor(color) {
-        super(color);
-        this.name = 'king';
-        this.symbol = (color === 'white') ? '♔' : '♚';
+    contructor(color, position, board) {
+        super(color, position, board, 'king');
     }
 
-    move(src, dest) {
-        return (src - 9 === dest ||
-            src - 8 === dest ||
-            src - 7 === dest ||
-            src + 1 === dest ||
-            src + 9 === dest ||
-            src + 8 === dest ||
-            src + 7 === dest ||
-            src - 1 === dest);
+    getPossibleMoves() {
+        const moves = [];
+        const kingMoves = [
+            { x: 1, y: 0 }, // right
+            { x: -1, y: 0 }, // left
+            { x: 0, y: 1 }, // down
+            { x: 0, y: -1 }, // up
+            { x: 1, y: 1 }, // down right
+            { x: -1, y: 1 }, // down left
+            { x: 1, y: -1 }, // up right
+            { x: -1, y: -1 } // up left
+        ];
+
+        kingMoves.forEach(offset => {
+            const newPos = {
+                x: this.position.x + offset.x,
+                y: this.position.y + offset.y
+            };
+
+            if (this.board.isPositionValid(newPos)) {
+                if (this.board.isSquareEmpty(newPos) || 
+                    this.board.squares[newPos.x][newPos.y].color !== this.color) {
+                    moves.push(newPos);
+                }
+            }
+        });
+
+        // Castle
+        if (!this.hasMoved && this.board.squares.checkstate[this.color]) {
+            // Castle short
+            const kingsideRook = this.noard.squares[this.position.x][7];
+            if (kingsideRook instanceof rook && !kingsideRook.hasMoved) {
+                const emptyBetween = [5, 6].every(y =>
+                    this.board.isSquareEmpty({x: this.position.x,y })
+                );
+                const notUnderAttack = [5, 6].every(y =>
+                    !this.board.isSquareUnderAttack({ x: this.position.x, y }, this.color === 'white' ? 'black' : 'white')
+                );
+            }
+            if (emptyBetween && notUnderAttack) {
+                moves.push({ x: this.position.x, y: 6 });
+            }
+            // Castle long
+            const queensideRook = this.board.squares[this.position.x][0];
+            if (queensideRook instanceof rook && !queensideRook.hasMoved) {
+                const emptyBetween = [1, 2, 3].every(y =>
+                    this.board.isSquareEmpty({ x: this.position.x, y })
+                );
+                const notUnderAttack = [1, 2].every(y =>
+                    !this.board.isSquareUnderAttack({ x: this.position.x, y }, this.color === 'white' ? 'black' : 'white')
+                );
+                if (emptyBetween && notUnderAttack) {
+                    moves.push({ x: this.position.x, y: 2 });
+                }
+            }
+        }
+        return moves;
     }
 }
-module.exports = king;

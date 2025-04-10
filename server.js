a21765e2aa9660cb2d39c8aa1629c9ab479de603
adrian.jaworski@ynov.com <adrian.jaworski@ynov.com>
2025-04-10 09:08:39 +0200

    ajouts pages

--- server.js
+++ server.js
@@ -6,7 +6,18 @@ const port = 8080
 
 const requestHandler = (request, response) => {
     console.log(request.url)
-    const filePath = path.join(__dirname, 'index.html')
+    let filPath = './index/menu.html' + request.url
+    if (request.url === '/') {
+        filPath = './index/menu.html'
+    } else if (request.url === '/about') {
+        filPath = './index/about.html'
+    } else if (request.url === '/contact') {
+        filPath = './index/contact.html'
+    } else {
+        filPath = './index/404.html'
+    }
+
+    response.writeHead(200, { 'Content-Type': 'text/html' })
 }
 
 const server = http.createServer(requestHandler)

80a515ea3d0603e608331625e3746fe880467262
adrian.jaworski@ynov.com <adrian.jaworski@ynov.com>
2025-04-10 10:53:25 +0200

    correction non fonctionnel

--- server.js
+++ server.js
@@ -5,19 +5,32 @@ const http = require('http')
 const port = 8080
 
 const requestHandler = (request, response) => {
-    console.log(request.url)
-    let filPath = './index/menu.html' + request.url
-    if (request.url === '/') {
-        filPath = './index/menu.html'
-    } else if (request.url === '/about') {
-        filPath = './index/about.html'
-    } else if (request.url === '/contact') {
-        filPath = './index/contact.html'
-    } else {
-        filPath = './index/404.html'
-    }
+    console.log("URL demandée :", request.url)
 
-    response.writeHead(200, { 'Content-Type': 'text/html' })
+    let filePath = './index'
+
+    // Gestion des routes
+    if (request.url === '/' || request.url === '/index') {
+        filePath += '/index.html'
+    } else if (request.url === '/about') {
+        filePath += '/about.html'
+    } else if (request.url === '/contact') {
+        filePath += '/contact.html'
+    } else {
+        filePath += '/404.html' 
+    }
+    const fs = require('fs')
+    const path = require('path')
+    // Lecture du fichier HTML
+    fs.readFile(path.join(__dirname, filePath), (err, data) => {
+        if (err) {
+            response.writeHead(500, { 'Content-Type': 'text/plain' })
+            response.end('Erreur du serveur')
+        } else {
+            response.writeHead(200, { 'Content-Type': 'text/html' })
+            response.end(data)
+        }
+    })
 }
 
 const server = http.createServer(requestHandler)

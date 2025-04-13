e3c164b2244f32d8ddc4c9af9b28584b0a0ba8d6
ORTHOMAS <thomas.orjubin@ynov.com>
2025-04-13 15:25:00 +0200

    Timer

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-const http = require('http')
-const port = 8080
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = 8080;
 
-const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end("Running")
-}
+const requestHandler = (_, res) => {
+    const filePath = path.join(__dirname, './timer.html');
 
-const server = http.createServer(requestHandler)
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Erreur interne du serveur');
+            return;
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
+};
+
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
-    if(err) {
-        return console.log("Error", err)
+    if (err) {
+        return console.log("Error", err);
     }
-    console.log(`Listening on port ${port}`)
-})
+    console.log(`Listening on http://localhost:${port}`);
+});

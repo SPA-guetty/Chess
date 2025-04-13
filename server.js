ec2e817b1db2454dbf3cc5426c41406032ed6846
adrian.jaworski@ynov.com <adrian.jaworski@ynov.com>
2025-04-13 16:12:28 +0200

    teste non valid

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const mimeTypes = {
     '.css': 'text/css',
     '.js': 'text/javascript',
 }
+const fs = require('fs')
+const path = require('path')
 const requestHandler = (request, response) => {
     console.log(request.url)
     let filPath = './index/menu.html' + request.url
@@ -20,8 +22,17 @@ const requestHandler = (request, response) => {
     } else {
         filPath = './index/404.html'
     }
-
-    response.writeHead(200, { 'Content-Type': 'text/html' })
+fs.readFile(filPath, (err, data) => {
+        if (err) {
+            response.writeHead(404, { 'Content-Type': 'text/html' })
+            return response.end('404 Not Found')
+        }
+        const extname = String(path.extname(filPath)).toLowerCase()
+        const contentType = mimeTypes[extname] || 'application/octet-stream'
+        response.writeHead(200, { 'Content-Type': contentType })
+        response.end(data, 'utf-8')
+    })
+   
 }
 
 const server = http.createServer(requestHandler)

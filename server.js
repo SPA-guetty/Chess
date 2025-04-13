2cac2be0fea03ae9fbce53cc52aa501a2d67a2ca
Chloe Alexandre <chloe.alexandre@ynov.com>
2025-04-13 18:42:38 +0200

    Fix merge issue

--- server.js
+++ server.js
@@ -1,19 +1,3 @@
-const http = require('http')
-const port = 8080
-
-const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end("Running")
-}
-
-const server = http.createServer(requestHandler)
-
-server.listen(port, (err) => {
-    if(err) {
-        return console.log("Error", err)
-    }
-    console.log(`Listening on port ${port}`)
-})
 //node serveur.js
 //http://localhost:8080/
 

25efa986a1ff080557e8883850ef2bbb6675bac3
adrian.jaworski@ynov.com <adrian.jaworski@ynov.com>
2025-04-13 18:15:58 +0200

    liaison page serveur okay

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 //node serveur.js
 //http://localhost:8080/
 
-const http = require('http')
-const port = 8080
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 8080;
+
 const mimeTypes = {
     '.html': 'text/html',
     '.css': 'text/css',
     '.js': 'text/javascript',
-}
-const fs = require('fs')
-const path = require('path')
+};
+
 const requestHandler = (request, response) => {
     console.log(request.url)
-    let filPath = './index/menu.html' + request.url
+    let filPath = './index' + request.url 
+
     if (request.url === '/') {
         filPath = './index/menu.html'
-    } else if (request.url === '/about') {
-        filPath = './index/about.html'
-    } else if (request.url === '/contact') {
-        filPath = './index/contact.html'
-    } else {
-        filPath = './index/404.html'
     }
-fs.readFile(filPath, (err, data) => {
+
+    fs.readFile(filPath, (err, data) => {
         if (err) {
+            console.log(err)
             response.writeHead(404, { 'Content-Type': 'text/html' })
             return response.end('404 Not Found')
         }
@@ -32,14 +32,14 @@ fs.readFile(filPath, (err, data) => {
         response.writeHead(200, { 'Content-Type': contentType })
         response.end(data, 'utf-8')
     })
-   
 }
 
-const server = http.createServer(requestHandler)
+
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
-    if(err) {
-        return console.log("Error", err)
+    if (err) {
+        return console.log('Error', err);
     }
-    console.log(`Listening on port ${port}`)
-})
+    console.log(`Listening on port ${port}`);
+});

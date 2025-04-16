898f7c4274cd10698cdaea70e02105c4ee9b3f42
adrian.jaworski@ynov.com <adrian.jaworski@ynov.com>
2025-04-16 15:11:34 +0200

    son

--- index/pionplateau/bishop.js
+++ index/pionplateau/bishop.js
@@ -3,6 +3,11 @@ class Bishop extends Piece {
         super(color, position, board, 'bishop');
     }
 
+    moveTo(newPosition) {
+        super.moveTo(newPosition); // appel de la logique de mouvement de base
+        this.moveSound.play('https://images.chesscomfiles.com/chess-themes/sounds/_MP3_/default/move-self.mp3');     // joue le son
+    }
+
     getPossibleMoves() {
         const directions = [
             { x: 1, y: 1 }, // down right
